af3826e1154460359dfd160cdda9aa64b3cba0e7
Thareesha98 <154692052+Thareesha98@users.noreply.github.com>
2024-12-26 19:01:22 +0530

    Update CartSlice.jsx with functionalities

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react';
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -7,13 +8,29 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const item=action.payload;
+      const existingItem=state.items.find((i) => i.name===item.name);
+      if(existingItem){
+        existingItem.quantity++;
+      }else{
+        state.items.push({...item,quantity:1});
+      }
     },
     removeItem: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find((x) => x.name === item.name);
+      if(existingItem){
+        state.items = state.items.filter((x) => x.name != item.name)
+      }
     },
     updateQuantity: (state, action) => {
-
-    
+      const item = action.payload;
+      const existingItem = state.items.find((x) => x.name === item.name);
+      existingItem.quantity = item.quantity;
+      //Problem heeree
+      if(existingItem.quantity ===0){
+        state.items = state.items.filter((x) => x.name !== item.name)
+      }
     },
   },
 });
